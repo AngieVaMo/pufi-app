98569df2bbdc40bde91b7daa148ad190627ded64
AngieVaMo <anvamopa2@gmail.com>
2022-09-29 12:23:26 -0500

    Home Component

---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+export default function Home(){
+    return(
+        <div>
+            <div>
+            <NavBar/>
+            </div>
+
+            <div>
+                <h3>ESTAR CÓMODO,</h3>
+                <h3>NUNCA FUE TAN FÁCIL.</h3>
+            </div>
+
+            <div>
+            <button>SHOP</button>
+            </div>
+        </div>
+    )
+}
